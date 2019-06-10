e2ffbf36d222ca647e039885fa9c4ef1ff21691d
Leigh London <leigh.00.london@gmail.com>
2019-06-10 21:11:54 +1000

    refactor to table-driven sub-benchmarks
    going wild with this stuff

--- semver_bench_test.go
+++ semver_bench_test.go
@@ -5,31 +5,34 @@ import (
 )
 
 func BenchmarkVersionToString(b *testing.B) {
-	v := Version{
-		Major: 30,
-		Minor: 5,
-		Patch: 2019,
-		Build: "766c5b54aae8d9de1708160817b93db1997e35cd",
+	cases := []struct {
+		name string
+		v    Version
+	}{
+		{"full", Version{Major: 30, Minor: 5, Patch: 2019, Build: "766c5b54aae8d9de1708160817b93db1997e35cd"}},
+		{"classic", Version{Major: 1, Minor: 2, Patch: 3}},
 	}
 
-	b.Run("builder", func(b *testing.B) {
-		for n := 0; n < b.N; n++ {
-			v.builder()
-		}
-	})
-	b.Run("builder2", func(b *testing.B) {
-		for n := 0; n < b.N; n++ {
-			v.builder2()
-		}
-	})
-	b.Run("sprintf", func(b *testing.B) {
-		for n := 0; n < b.N; n++ {
-			v.sprintf()
-		}
-	})
-	b.Run("sprintf2", func(b *testing.B) {
-		for n := 0; n < b.N; n++ {
-			v.sprintf2()
-		}
-	})
+	for _, c := range cases {
+		b.Run("builder/"+c.name, func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				c.v.builder()
+			}
+		})
+		b.Run("builder2/"+c.name, func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				c.v.builder2()
+			}
+		})
+		b.Run("sprintf/"+c.name, func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				c.v.sprintf()
+			}
+		})
+		b.Run("sprintf2/"+c.name, func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				c.v.sprintf2()
+			}
+		})
+	}
 }
