49999052a2fb9d396a83a3c64417a74cee99fcb6
Leigh London <leigh.00.london@gmail.com>
2019-06-10 14:44:43 +1000

    bench both versions

--- semver_bench_test.go
+++ semver_bench_test.go
@@ -12,6 +12,18 @@ func BenchmarkVersionToString(b *testing.B) {
 		Build: "766c5b54aae8d9de1708160817b93db1997e35cd",
 	}
 	for n := 0; n < b.N; n++ {
-		version.String()
+		version.stringBuilder()
+	}
+}
+
+func BenchmarkVersionToStringSprintf(b *testing.B) {
+	version := Version{
+		Major: 30,
+		Minor: 5,
+		Patch: 2019,
+		Build: "766c5b54aae8d9de1708160817b93db1997e35cd",
+	}
+	for n := 0; n < b.N; n++ {
+		version.sprintf()
 	}
 }
