6fd501548326b89863de6c823ccaf5f8fc5ac320
Leigh London <leigh.00.london@gmail.com>
2019-06-10 21:01:17 +1000

    fix typo

--- semver_bench_test.go
+++ semver_bench_test.go
@@ -12,7 +12,7 @@ func BenchmarkVersionToString(b *testing.B) {
 		Build: "766c5b54aae8d9de1708160817b93db1997e35cd",
 	}
 
-	b.Run("builder1", func(b *testing.B) {
+	b.Run("builder", func(b *testing.B) {
 		for n := 0; n < b.N; n++ {
 			v.builder()
 		}
